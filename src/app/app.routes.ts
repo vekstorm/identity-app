2e9ba133b4d6222de75eaf4d42869cc622403179
dteso <dtesodev@gmail.com>
2026-06-18 16:35:42 +0200

    routes lazy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import("./components/home/home").then((m) => m.Home)
+  },
+  {
+    path: 'authorized',
+    loadComponent: () => import("./components/authorized/authorized").then((m) => m.Authorized)
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
